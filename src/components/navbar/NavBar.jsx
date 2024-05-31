a0be74e6064a4f35b10909e0ffc7a81eedc5f361
samvelsargsyan95 <samvelsargsyan0995@gmail.com>
2024-05-31 22:09:14 +0400

    Bug fixes

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,6 +1,6 @@
 import './NavBar.scss';
 import Menu from '../menu/Menu';
 
-export default function NavBar({ isNavbarHidden }) {
+export default function NavBar() {
     return ( <Menu parentName="navbar" showArrows={true} /> )
 }
